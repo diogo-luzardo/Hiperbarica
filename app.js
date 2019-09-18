94c46a74eb3cdefb96847f6aec9d9ef3d25cc86e
Diogo Luzardo Pereira <diogoluzardo@gmail.com>
2019-09-18 16:38:36 -0300

    confingurado nav

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const navSlide = () => {
+    const toggle = document.querySelector('.toggle')
+    const nav = document.querySelector('.nav-links')
 
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    })
+}
+
+navSlide();
