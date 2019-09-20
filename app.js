465c76f71e293db7cbcdab0bba1ae9eca523b0bf
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-09-20 09:11:37 -0300

    Adionando responsividade

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const navSlide = () => {
     const toggle = document.querySelector('.toggle')
     const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li')
 
     toggle.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`
+            }
+         })
+         toggle.classList.toggle('line');
     })
+
 }
 
 navSlide();
